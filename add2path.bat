273aad691c2dea0fc45a6010462e7e015be9438f
Frogg <admin@frogg.fr>
2018-03-25 16:51:56 +0200

    Added add2path file
    added main script

---
+++ add2path.bat
@@ -0,0 +1,79 @@
+@echo off
+REM             _ __ _
+REM         ((-)).--.((-))
+REM         /     ''     \
+REM        (   \______/   )
+REM         \    (  )    /
+REM         / /~~~~~~~~\ \
+REM    /~~\/ /          \ \/~~\
+REM   (   ( (            ) )   )
+REM    \ \ \ \          / / / /
+REM    _\ \/  \.______./  \/ /_
+REM    ___/ /\__________/\ \___
+REM   *****************************
+
+REM Script call :
+REM -------------
+REM add2path.bat
+
+REM description : 
+REM -------------
+REM will add to path the DefaultList defined in the file
+
+REM OR
+
+REM Script call :
+REM -------------
+REM add2path.bat C:\dev\bin\git\bin C:\dev\bin\php C:\dev\bin\composer C:\dev\bin\ruby\bin C:\dev\bin\node
+
+REM description : 
+REM -------------
+REM will add to path the list in argument
+
+REM Default List if nothing set as arguments
+set DefaultList=C:\dev\bin\git\bin C:\dev\bin\php C:\dev\bin\composer C:\dev\bin\ruby\bin C:\dev\bin\node
+
+REM GLOBAL CONFIG VARS
+REM List of folders to add to the PATH (Array separated by spaces !)
+REM ================================================================
+REM If bin path are passed as argument use them, else use LIST2ADD var
+if "%*"=="" (set LIST2ADD=%DefaultList%)
+else(set LIST2ADD=%*)
+REM
+REM default list = git, php, composer, ruby, node
+REM ================================================================
+
+REM PREPARATION : Reset & init vars
+REM -------------------------------
+set "formatedPath="
+set "originalPath=%PATH%"
+
+REM STEP 1 : Display information to user
+REM ------------------------------------
+REM Clean screen
+cls
+REM ask user
+echo You are going to add to the PATH the following folders (press Ctrl+C to cancel) :
+REM Display list of folders to add to path
+(for %%f in (%LIST2ADD%) do (
+   echo - %%f
+)) 
+REM Make a pause
+echo. 
+pause
+echo. 
+
+REM STEP 2 : Formating informations
+REM -------------------------------
+REM Adding to path but check if already exist
+(for %%a in (%LIST2ADD%) do (
+REM Doing concatenation to formatedPath var with list of path to add
+call set "formatedPath=%%formatedPath%%%%a;"
+REM removing path if already exist to not add it multiple times
+call set "originalPath=%%originalPath:%%a;=%%"
+))
+
+REM STEP 3 : Updating the PATH
+REM --------------------------
+set PATH=%originalPath%%formatedPath%
+setx PATH %originalPath%%formatedPath%
